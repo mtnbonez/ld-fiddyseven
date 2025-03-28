2ceced36f5f7fae7b0398104b592d3c71c9dfc47
mtnbonez <ferrell.w.a@gmail.com>
2025-03-27 23:11:08 -0500

    Initial commit for project creation
    ya heard

---
+++ unity/ld-fiddyseven/Assets/Scwipts/YeOldeCode.cs
@@ -0,0 +1,24 @@
+// Put established coding standards in here for easy reference.
+
+using System;
+using UnityEngine;
+
+// Our namespace
+namespace ld57
+{
+    // Add ur name in here so I know you installed VStudio 2022 correctly :)
+    public enum Participants
+    {
+    }
+
+
+    public class WePascalCasing
+    {
+        public void Functions()
+        {
+            return;
+        }
+
+    }
+}
+
