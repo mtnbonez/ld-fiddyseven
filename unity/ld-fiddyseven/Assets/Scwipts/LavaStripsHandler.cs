1f6bfb1e00b38c6c0a4191cf38487d16b3374bca
Walter Ferrell <ferrell.w.a@gmail.com>
2025-04-05 16:18:07 -0500

    fixed lava :smile:

--- unity/ld-fiddyseven/Assets/Scwipts/LavaStripsHandler.cs
+++ unity/ld-fiddyseven/Assets/Scwipts/LavaStripsHandler.cs
@@ -1,19 +1,7 @@
 using UnityEngine;
 
 public class LavaStripsHandler : MonoBehaviour
-{
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
+{   
     private void OnCollisionEnter(Collision collision)
     {
         Debug.Log("collided");
