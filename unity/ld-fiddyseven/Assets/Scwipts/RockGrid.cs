ac9521cc1be415c436698f6447f89b06121f0803
jjak415 <jjake1232@gmail.com>
2025-04-05 01:33:52 -0400

    variable rock grid sizes

--- unity/ld-fiddyseven/Assets/Scwipts/RockGrid.cs
+++ unity/ld-fiddyseven/Assets/Scwipts/RockGrid.cs
@@ -4,7 +4,8 @@ public class RockGrid : MonoBehaviour
 {
 
     [SerializeField] GameObject rocks;
-    [SerializeField] int gridSize = 64;
+    [SerializeField] int xGridSize = 64;
+    [SerializeField] int yGridSize = 1;
     [SerializeField] float spacing = 1f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -14,9 +15,9 @@ public class RockGrid : MonoBehaviour
 
     void MakeRocks()
     {
-        for (int x = 0; x < gridSize; x++)
+        for (int x = 0; x < xGridSize; x++)
         {
-            for (int y = 0; y < gridSize; y++)
+            for (int y = 0; y < yGridSize; y++)
             {
                 Vector3 position = new Vector3(x * spacing, -y * spacing, 0);
                 Instantiate(rocks, position, Quaternion.identity, transform);
