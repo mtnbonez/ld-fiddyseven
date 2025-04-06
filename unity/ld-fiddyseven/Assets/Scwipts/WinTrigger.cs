e1c7572f68d3ba97a2675793962b7c3a2ad3480d
Ben <bbickl22@gmail.com>
2025-04-06 14:59:51 -0700

    Removing alt tags on rocks so we can use enum; set wintrigger to not trigger on lava

--- unity/ld-fiddyseven/Assets/Scwipts/WinTrigger.cs
+++ unity/ld-fiddyseven/Assets/Scwipts/WinTrigger.cs
@@ -5,8 +5,14 @@ public class WinTrigger : MonoBehaviour
 
     public void OnCollisionEnter(Collision collision)
     {
-        GameManager.Instance.LevelCompleted += 1;
-        GameManager.Instance.SwitchScene_Shop();
+
+        if (!collision.gameObject.TryGetComponent<LavaStripsHandler>(out LavaStripsHandler test))
+        {
+            GameManager.Instance.LevelCompleted += 1;
+            GameManager.Instance.SwitchScene_Shop();
+            Debug.Log("Level trigger was hit by " + collision.gameObject.name);
+        }
+
     } 
 
 }
