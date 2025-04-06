fa09b8ede3445c567c0b2b6e7a019ce627d68899
Matt Grubb <31322395+mattgrubb1@users.noreply.github.com>
2025-04-05 20:43:16 -0700

    Populating shop UI based on shop inventory

--- unity/ld-fiddyseven/Assets/Scwipts/Buff.cs
+++ unity/ld-fiddyseven/Assets/Scwipts/Buff.cs
@@ -8,22 +8,36 @@ public class Buff
         TEST_BUFF_2,
         TEST_BUFF_3,
         TEST_BUFF_4,
+        TEST_BUFF_5,
     }
 
-    private Dictionary<BUFF_TYPE, string> BuffList = new Dictionary<BUFF_TYPE, string> { 
-        {BUFF_TYPE.TEST_BUFF_1, "Test Buff 1"},
-        {BUFF_TYPE.TEST_BUFF_2, "Test Buff 2"},
-        {BUFF_TYPE.TEST_BUFF_3, "Test Buff 3"},
-        {BUFF_TYPE.TEST_BUFF_4, "Test Buff 4"},
+    private static readonly Dictionary<BUFF_TYPE, BuffData> BuffList = new Dictionary<BUFF_TYPE, BuffData> { 
+        {BUFF_TYPE.TEST_BUFF_1, new BuffData("Test Buff 1", 100)},
+        {BUFF_TYPE.TEST_BUFF_2, new BuffData("Test Buff 2", 200)},
+        {BUFF_TYPE.TEST_BUFF_3, new BuffData("Test Buff 3", 300)},
+        {BUFF_TYPE.TEST_BUFF_4, new BuffData("Test Buff 4", 400)},
+        {BUFF_TYPE.TEST_BUFF_5, new BuffData("Test Buff 5", 500)},
     };
     
-    public string GetBuffString(BUFF_TYPE buffType )
+    public static BuffData GetBuffData(BUFF_TYPE buffType )
     {
         return BuffList[buffType];
     }
 
-    public Dictionary<BUFF_TYPE, string> GetBuffList()
+    public Dictionary<BUFF_TYPE, BuffData> GetBuffList()
     {
         return BuffList;
+    }
+}
+
+public struct BuffData
+{
+    public string buffName;
+    public int buffCost;
+
+    public BuffData( string name, int cost )
+    {
+        buffName = name;
+        buffCost = cost;
     }
 }
