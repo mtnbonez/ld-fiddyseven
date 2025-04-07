9dd24bd9bc046a5eb7e8229d8f1a2076a7dfbcde
Walter Ferrell <ferrell.w.a@gmail.com>
2025-04-07 13:23:15 -0500

    fixing broken shit

--- unity/ld-fiddyseven/Assets/ShopIntroHandler.cs
+++ unity/ld-fiddyseven/Assets/ShopIntroHandler.cs
@@ -3,10 +3,7 @@ using UnityEngine;
 public class ShopIntroHandler : MonoBehaviour
 {
     public GameObject dialogBox;
-<<<<<<< Updated upstream
     public bool HideAfterExit = false;
-=======
->>>>>>> Stashed changes
 
     private bool hasTriggered = false;
 
@@ -14,13 +11,6 @@ public class ShopIntroHandler : MonoBehaviour
     {
         if (collider.tag == "playerCollision" && !hasTriggered)
         {
-<<<<<<< Updated upstream
-=======
-            Debug.Log("Test");
-            // Enable dialog box
-            // TODO: add dialog box as well
-
->>>>>>> Stashed changes
             if (dialogBox != null)
             {
                 dialogBox.SetActive(true);
@@ -32,7 +22,6 @@ public class ShopIntroHandler : MonoBehaviour
 
     private void OnTriggerExit(Collider other)
     {
-<<<<<<< Updated upstream
         if (HideAfterExit)
         {
             if (other.tag == "playerCollision" && hasTriggered)
@@ -41,15 +30,6 @@ public class ShopIntroHandler : MonoBehaviour
                 {
                     dialogBox.SetActive(false);
                 }
-=======
-        if (other.tag == "playerCollision" && hasTriggered)
-        {
-            Debug.Log("Test2");
-
-            if (dialogBox != null)
-            {
-                dialogBox.SetActive(false);
->>>>>>> Stashed changes
             }
         }
     }
