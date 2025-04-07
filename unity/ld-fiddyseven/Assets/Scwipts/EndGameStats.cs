87c34ed13811b2f9b4e2179119ef4344df6c4dfc
Ben <bbickl22@gmail.com>
2025-04-07 14:46:37 -0700

    Adding endgame stats

---
+++ unity/ld-fiddyseven/Assets/Scwipts/EndGameStats.cs
@@ -0,0 +1,22 @@
+using TMPro;
+using UnityEngine;
+
+public class EndGameStats : MonoBehaviour
+{
+    StatsManager statsManager = GameManager.Instance.GetStatsManager();
+    public GameObject GoldCounter;
+    public GameObject GoldSpent;
+    public GameObject RocksBusted;
+
+    void OnEnable()
+    {
+        string goldEarned = statsManager.playerStats.GoldEarned.ToString();
+        string GoldSpent = statsManager.playerStats.GoldSpent.ToString();
+        string RocksBusted = statsManager.playerStats.NormalRocksBroken.ToString(); 
+
+        GoldCounter.GetComponent<TextMeshProUGUI>().text = goldEarned;
+        GoldCounter.GetComponent<TextMeshProUGUI>().text = GoldSpent;
+        GoldCounter.GetComponent<TextMeshProUGUI>().text = RocksBusted;
+    }
+
+}
